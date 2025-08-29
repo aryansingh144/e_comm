cad5bf3a77c845bdda8f0e9efaf2e2b9a5a6c436
your-username <aryansingh7910@gmail.com>
2025-08-30 00:25:13 +0530

    few errors fixed

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import { createContext } from 'react'
-export const authDataContext= createContext()
-function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+import React, { createContext } from "react";
 
-    let value = {
-       serverUrl
-    }
+export const authDataContext = createContext();
+
+function AuthContext({ children }) {
+  let serverUrl = "http://localhost:8000";
+
+  let value = {
+    serverUrl,
+  };
+
   return (
-
-    
-    <div>
-        <authDataContext.Provider value={value}>
-            {children}
-        </authDataContext.Provider>
-      
-    </div>
-  )
+    <authDataContext.Provider value={value}>
+      {children}
+    </authDataContext.Provider>
+  );
 }
 
-export default AuthContext
+export default AuthContext;
